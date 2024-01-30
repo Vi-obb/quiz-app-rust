aa033471ff154c0d4899756f1e960034ef4caec1
7heMagician <obbengvincent@gmail.com>
2024-01-30 11:45:32 +0000

    Refactor UI styling and add button customization

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 mod quiz;
 
 fn main() -> Result<(), eframe::Error> {
-    let options = eframe::NativeOptions::default();
+    let viewport = egui::ViewportBuilder::default().with_inner_size(egui::Vec2::new(500.0, 500.0));
+
+    let options = eframe::NativeOptions {
+        viewport,
+        ..Default::default()
+    };
     eframe::run_native("QuizMe", options, Box::new(|_ctx| Box::new(App::new())))
 }
 
@@ -19,6 +24,18 @@ impl App {
 impl eframe::App for App {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
+            let mut style = (*ctx.style()).clone();
+
+            style.text_styles = [
+                (egui::TextStyle::Body, egui::FontId::proportional(18.0)),
+                (egui::TextStyle::Button, egui::FontId::proportional(18.0)),
+            ]
+            .into();
+
+            style.spacing.button_padding = egui::Vec2::new(10.0, 5.0);
+
+            ui.set_style(style);
+
             let current_index = self.quiz.current_index + 1;
             let count = self.quiz.questions.len();
             let score = self.quiz.calculate_score();
@@ -30,51 +47,68 @@ impl eframe::App for App {
 
             // function to get the current question
             let current_question = self.quiz.current_question();
-            ui.label(&current_question.title);
+            ui.label(
+                egui::RichText::new(current_question.title.as_str())
+                    .font(egui::FontId::proportional(32.0)),
+            );
 
             // Buttons
             ui.horizontal(|ui| {
-                if ui.button("True").clicked() {
+                let true_button = get_button(
+                    "true",
+                    self.quiz.current_question().user_answer == Some(true),
+                );
+
+                let false_button = get_button(
+                    "false",
+                    self.quiz.current_question().user_answer == Some(false),
+                );
+
+                if ui.add(true_button).clicked() {
                     self.quiz.answer(true);
                 }
-                if ui.button("False").clicked() {
+
+                if ui.add(false_button).clicked() {
                     self.quiz.answer(false);
-                }
-
-                let current_question = self.quiz.current_question();
-                if let Some(_user_answer) = current_question.user_answer {
-                    if current_question.is_correct() {
-                        ui.label("Correct!");
-                    } else {
-                        ui.label("Incorrect!");
-                    }
-                }
+                };
             });
 
-            ui.horizontal(|ui| {
-                if ui.button("Previous").clicked() {
-                    self.quiz.prev_question();
-                }
-                if ui.button("Next").clicked() {
-                    self.quiz.next_question();
-                }
+            ui.columns(2, |columns| {
+                columns[0].allocate_ui_with_layout(
+                    egui::Vec2 { x: 120.0, y: 40.0 },
+                    egui::Layout::left_to_right(egui::Align::Center),
+                    |ui| {
+                        if ui.button("Previous").clicked() {
+                            self.quiz.prev_question();
+                        }
+                    },
+                );
+                columns[1].allocate_ui_with_layout(
+                    egui::Vec2 { x: 120.0, y: 40.0 },
+                    egui::Layout::right_to_left(egui::Align::Center),
+                    |ui| {
+                        if ui.button("Next").clicked() {
+                            self.quiz.next_question();
+                        }
+                    },
+                );
             });
-            // Question count and score
-
-            // egui::CentralPanel::default().show(ctx, |ui| {
-            //     ui.label("1/10");
-            //     ui.label("What is the capital of Nigeria?");
-
-            //     ui.horizontal(|ui| {
-            //         ui.button("True");
-            //         ui.button("False");
-            //     });
-
-            //     ui.horizontal(|ui| {
-            //         ui.button("Previous");
-            //         ui.button("Next");
-            //     });
-            // });
         });
     }
+}
+
+fn get_button(label: &str, selected: bool) -> egui::Button<'static> {
+    let mut label = egui::RichText::from(label);
+
+    if selected {
+        label = label.color(egui::Color32::WHITE);
+    }
+
+    let mut button = egui::Button::new(label).min_size(egui::Vec2::new(60.0, 30.0));
+
+    if selected {
+        button = button.fill(egui::Color32::BLUE);
+    }
+
+    button
 }
